5b95990c5f027a168539d32554f2e98629e4e204
moohamed-ashraf <113698376+moohamed-ashraf@users.noreply.github.com>
2024-09-05 15:08:16 +0300

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClient],
+  imports: [CommonModule, HttpClientModule],
 })
 export class CoreModule {}
