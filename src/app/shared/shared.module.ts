4e31b9bdcefcb6049d76249a8ceed045021db31d
fatma lotfy <fatma.lotfy292@gmail.com>
2024-09-11 19:44:03 +0300

    home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MobileApplicationComponent } from './mobile-application/mobile-application.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, FooterComponent, MobileApplicationComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [NavbarComponent, FooterComponent, MobileApplicationComponent],
 })
 export class SharedModule {}
