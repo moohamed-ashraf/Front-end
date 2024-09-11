6eec584359247df96f21e1de2ec3d8e279c275a4
Mohamed Ashraf <mohamedmokhtar1847@gmail.com>
2024-09-11 14:31:10 +0300

    ui update

--- src/app/modules/user-account/user-account/pages/profile/my-profile/my-profile.component.ts
+++ src/app/modules/user-account/user-account/pages/profile/my-profile/my-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContentComponent } from '../../../components/content/content.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
