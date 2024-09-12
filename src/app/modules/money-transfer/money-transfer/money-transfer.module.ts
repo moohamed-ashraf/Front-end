fb3e4e9fc257292928355b309ede7814a5ec2d24
Mohamed Ashraf <mohamedmokhtar1847@gmail.com>
2024-09-12 12:01:07 +0300

    money transfer route

--- src/app/modules/money-transfer/money-transfer/money-transfer.module.ts
+++ src/app/modules/money-transfer/money-transfer/money-transfer.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // Import RouterModule
 import { MoneyTransferRoutingModule } from './money-transfer-routing.module';
-import { MoneyTransferComponent } from 'C:/Users/Mohamed Hesham/OneDrive/Desktop/finalproject/speedo-transfer-front-end/src/app/money-transfer/money-transfer.component';
-
+import { MoneyTransferComponent } from '../../../money-transfer/money-transfer.component';
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule, // Ensure RouterModule is imported
-    MoneyTransferRoutingModule,
-  ],
-  // No declarations array here
+  imports: [CommonModule, RouterModule, MoneyTransferRoutingModule],
 })
 export class MoneyTransferModule {}
--- src/app/modules/user-account/user-account/user-account-routing.module.ts
+++ src/app/modules/user-account/user-account/user-account-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MoneyTransferComponent } from '../../../money-transfer/money-transfer.component';
+const routes: Routes = [{ path: '', component: MoneyTransferComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
